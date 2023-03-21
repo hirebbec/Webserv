4640bd758bc5d8a9ecd0b74810ddebba4ebc72e4
Ruzel Fahraziev <reek867@gmail.com>
2023-03-22 00:56:24 +0300

    VSE?

--- developer_files/includes/Webserv.hpp
+++ developer_files/includes/Webserv.hpp
@@ -11,7 +11,6 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <fstream>
 
 
 void parseURI(std::vector<std::string>& envp, std::string& uri);
@@ -102,30 +101,36 @@ private:
 	}
 
 	bool readRequest(int sock) {
-		char buffer[BUFFER_SIZE];
-		int bytes = recv(sock, buffer, BUFFER_SIZE, 0);
-
-		if (bytes == 0) {
-			std::string response = httpResponse.generateResponse(204, std::vector<std::string>(), "/error_page/204.html");
-			send(sock, response.c_str(), response.length(), 0); // No content (can't check :( )
-			close(sock);
-			FD_CLR(sock, &_active_set);
-			return false;
-		}
+		char buf[BUFFER_SIZE];
+		int bytes = recv(sock, buf, BUFFER_SIZE, 0);
+		// std::cout << (const char*)buf;
 		if (bytes < 0) {
 			std::cerr << "Recv error\n";
 			close(sock);
 			FD_CLR(sock, &_active_set);
 			return false;
+		} else if (bytes == 0) {
+			std::string response = httpResponse.generateResponse(204, std::vector<std::string>(), "/error_page/204.html");
+			if (send(sock, response.c_str(), response.length(), 0) <= 0) {
+				delete_client(sock);
+				FD_CLR(sock, &_active_set);
+				close(sock);
+			} // No content (can't check :( )
+			close(sock);
+			FD_CLR(sock, &_active_set);
+			return false;
 		}
-		if (!httpParser.parse(buffer, bytes)) {
+		if (!httpParser.parse(buf, bytes)) {
 			std::string response = httpResponse.generateResponse(400, std::vector<std::string>(), "/error_page/400.html");
-			send(sock, response.c_str(), response.length(), 0); // Bad request (Checked)
+			if (send(sock, response.c_str(), response.length(), 0) <= 0) {
+				delete_client(sock);
+				FD_CLR(sock, &_active_set);
+				close(sock);
+			} // Bad request (Checked)
 			return false;
 		}
 		return true;
 	}
-
 
 	void sendAnswer(int sock) {
 		int code;
@@ -144,11 +149,15 @@ private:
 			if (httpParser.uri[httpParser.uri.length() - 1] == '/') { // case 2.3.1
 				if (exist(conf._root + httpParser.uri + conf._index)) { // case 2.3.1.1 (Checked)
 					std::string response = httpResponse.generateResponse(200, headers, conf._root + httpParser.uri + conf._index);
-					send(sock, response.c_str(), response.length(), 0);
+					if (send(sock, response.c_str(), response.length(), 0) <= 0) {
+						delete_client(sock);
+						FD_CLR(sock, &_active_set);
+						close(sock);
+					}
 					return;
 				} else if (conf._autoindex) { // case 2.3.1.2 (Checked)
 					generate_autoindex("." + conf._root + httpParser.uri, sock); // autoindex
-					return;g
+					return;
 				} else { // case 2.3.1.3 (Checked)
 					code = 404; // Not Found
 				}
@@ -197,8 +206,14 @@ private:
 			}
 		}
 		path = getPath(code, conf);
+		// std::cout << path << std::endl;
 		std::string response = httpResponse.generateResponse(code, headers, path);
-		send(sock, response.c_str(), response.length(), 0);
+		// std::cout << response;
+		if (send(sock, response.c_str(), response.length(), 0) <= 0) {
+			delete_client(sock);
+			FD_CLR(sock, &_active_set);
+			close(sock);
+		}
 	}
 
 	Server &choseServer(int sock) {
@@ -308,7 +323,11 @@ private:
 		response.append("\r\n");
 		response.append("\r\n");
 		response.append(ss.str());
-		send(sock, response.c_str(), response.length(), 0);
+		if (send(sock, response.c_str(), response.length(), 0) <= 0) {
+			delete_client(sock);
+			FD_CLR(sock, &_active_set);
+			close(sock);
+		}
 	}
 
 	std::string saveContent(std::string root) {
@@ -333,36 +352,31 @@ private:
 		return ss.str();
 	}
 
-	void executeScript(const std::string& uri, int sock) {
-		std::string path = "." + uri;
+	void	executeScript(std::string uri, int sock) {
+		uri = "." + uri;
 		int pid = fork();
 		if (pid == 0) {
 			dup2(sock, STDOUT_FILENO);
-			if (path.substr(path.length() - 3, 3) == ".py") {
+			if (uri.substr(uri.length() - 3, 3) == ".py") {
 				std::string python_path;
 				std::vector<std::string>::iterator it;
 				for (it = _paths.begin(); it != _paths.end(); ++it) {
-					std::ifstream python_file((*it) + "/python3");
-					if (python_file.good()) {
+					if (!access(((*it) + "/python3").c_str(), X_OK)) {
 						python_path = (*it) + "/python3";
 						break;
 					}
 				}
 				if (it != _paths.end()) {
-					char* argv[] = {
-						const_cast<char*>(python_path.c_str()),
-						const_cast<char*>(uri.c_str()),
-						NULL
-					};
+					char* argv[] = {const_cast<char*>(python_path.c_str()), const_cast<char*>(uri.c_str()), NULL};
 					execve(python_path.c_str(), argv, vectorToCharArray(_env));
 				}
 			} else {
-				execve(path.c_str(), NULL, NULL);
+				
+				execve(uri.c_str(), NULL, NULL);
 			}
 		}
 		wait(NULL);
 	}
-
 
 	
 };
